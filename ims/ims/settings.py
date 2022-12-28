e0e03dafe5ac872e1fe604dc5cb122939dd8e663
Khushnud <bokiev.khushnud@mail.ru>
2022-12-29 00:50:31 +0600

    Getting ready for deployment updated

--- ims/ims/settings.py
+++ ims/ims/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,7 +134,7 @@ MEDIA_URL = '/media/'
 
 # Static
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
