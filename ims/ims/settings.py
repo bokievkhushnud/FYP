548fb696c033f8859fae4185433153c5a2560101
Khushnud <bokiev.khushnud@mail.ru>
2022-12-29 01:34:52 +0600

    new corrections

--- ims/ims/settings.py
+++ ims/ims/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'ims.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -92,16 +92,16 @@ WSGI_APPLICATION = 'ims.wsgi.application'
 # }
 
 # connecting to posgresql
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'ims',
-       'USER': 'imsuser',
-       'PASSWORD': 'p@ssw0rd',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'ims',
+#        'USER': 'imsuser',
+#        'PASSWORD': 'p@ssw0rd',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
 
 
 
