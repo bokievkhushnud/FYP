b7d0452451b252d239c1f54748fd9eb6c51dd01a
Khushnud <bokiev.khushnud@mail.ru>
2022-12-29 01:57:56 +0600

    new corrections

--- ims/ims/settings.py
+++ ims/ims/settings.py
@@ -143,9 +143,9 @@ MEDIA_URL = '/media/'
 # Static
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
