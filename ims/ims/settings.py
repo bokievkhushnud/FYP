51bee0dc141cc80c277825b6a24d12b3e0d9ad88
Khushnud <bokiev.khushnud@mail.ru>
2022-12-29 12:06:30 +0600

    DB settings updated

--- ims/ims/settings.py
+++ ims/ims/settings.py
@@ -76,20 +76,6 @@ WSGI_APPLICATION = 'ims.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'ims',
-#     }
-# }
-
 # connecting to posgresql
 DATABASES = {
    'default': {
