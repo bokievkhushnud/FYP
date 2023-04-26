964aab73e02988736b92dc4299a705cdaca8c332
Khushnud <bokiev.khushnud@mail.ru>
2023-04-27 01:33:16 +0600

    fix: fix the settings.py

--- www/custom_storages.py
+++ www/custom_storages.py
@@ -1,6 +1,6 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class PublicMediaStorage(S3Boto3Storage):
-    def __init__(self, *args, **kwargs):
-        kwargs["default_acl"] = "public-read"
-        super().__init__(*args, **kwargs)
+    location = 'public'
+    default_acl = 'public-read'
+    file_overwrite = False
