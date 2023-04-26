4f027ac39621cc84eb026e7b6e3fa561335af3a6
Khushnud <bokiev.khushnud@mail.ru>
2023-04-27 03:32:12 +0600

    feat: add aws s3 bucket configurations

--- ims/settings.py
+++ ims/settings.py
@@ -193,7 +193,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # MEDIA_URL = '/media/'
 
 # Static
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -255,7 +255,7 @@ AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
-STATIC_URL = AWS_URL + '/static/'
+# STATIC_URL = AWS_URL + '/static/'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = AWS_URL + '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
