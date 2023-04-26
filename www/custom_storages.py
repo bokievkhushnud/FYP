7b354139891167b53e29801862d48bfd026c3d81
Khushnud <bokiev.khushnud@mail.ru>
2023-04-27 04:41:10 +0600

    fix: aws

--- www/custom_storages.py
+++ www/custom_storages.py
@@ -2,5 +2,5 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    location = 'media'
     default_acl = 'public-read'
