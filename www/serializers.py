314453decb110619edda86894960e8423bc69247
Khushnud <bokiev.khushnud@mail.ru>
2023-04-28 01:07:27 +0600

    fix: fix the serializer

--- www/serializers.py
+++ www/serializers.py
@@ -7,13 +7,13 @@ from django.contrib.auth.models import User
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ['id', 'item_name', 'image', 'quantity', 'price']
+        fields = ['id', 'item_name', 'image', 'quantity', 'price','currency', 'description', 'vendor', 'date_received', 'expiration_date', 'notes', 'order_number', 'status']
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ['first_name', 'last_name', 'email', 'username']
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = UserSerializer()
