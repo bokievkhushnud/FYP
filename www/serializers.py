e4a9ce19d53ab9dfac8bc6c1d7b0733fb00d1aa6
Khushnud <bokiev.khushnud@mail.ru>
2023-04-29 02:41:43 +0600

    feat: implement broken item feature.

--- www/serializers.py
+++ www/serializers.py
@@ -10,7 +10,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'username']
+        fields = ['first_name', 'last_name', 'email']
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
