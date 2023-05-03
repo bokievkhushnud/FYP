eebe8570bb27892b72af4437286f92913ffbc8b8
Khushnud <bokiev.khushnud@mail.ru>
2023-05-04 03:23:10 +0600

    fix: the profile creation

--- www/signals.py
+++ www/signals.py
@@ -10,11 +10,24 @@ def add_qr_item(sender, instance, **kwargs):
     data = instance.id
     # data = f"https://invenotry-ms.herokuapp.com/item/detail/{instance.id}/"
     img_url = generate_qr(instance.item_type, data, instance.id)
-    item_code = generate_code(instance.campus, instance.department, instance.category, instance.id)
-    Item.objects.filter(id=instance.id).update(qr_code=img_url, item_code=item_code)
+    item_code = generate_code(
+        instance.campus, instance.department, instance.category, instance.id)
+    Item.objects.filter(id=instance.id).update(
+        qr_code=img_url, item_code=item_code)
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created and instance.is_active:
+        # Extract data first name and last name from the email
+        first_name, last_name = instance.username.split('@')[0].split('.')
+        if "_" in last_name:
+            last_name = last_name.split("_")[0]
+        instance.email = instance.username
+        instance.first_name = first_name
+        instance.last_name = last_name
+        instance.save()
+
         Profile.objects.create(owner=instance)
+
+        
